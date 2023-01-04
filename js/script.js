c4d32a17772bd5bb5ec3fb483986c8568c14253e
Jakub Wilk <wilkjakub64@gmail.com>
2023-01-05 00:53:52 +0100

    Basic styling - trying out some stuff

--- js/script.js
+++ js/script.js
@@ -12,3 +12,9 @@ fetch('https://my.api.mockaroo.com/website_entries.json?key=7d9d28a0')
   .catch(error => {
     console.log("fetch error")
   })
+
+cursor = document.querySelector("#cursor")
+document.querySelector("body").onmousemove = (e) => {
+    cursor.style.top = `${e.clientY - cursor.offsetHeight / 2}px`
+    cursor.style.left = `${e.clientX - cursor.offsetWidth / 2}px`
+}
