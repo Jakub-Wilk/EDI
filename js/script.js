a45d5f0f19d96ef88983a861e1f03f5b4e1d3027
Jakub Wilk <wilkjakub64@gmail.com>
2023-01-05 20:52:32 +0100

    Add function to create nodes for data

--- js/script.js
+++ js/script.js
@@ -1,20 +1,78 @@
-fetch('https://my.api.mockaroo.com/website_entries.json?key=7d9d28a0')
-  .then(response => {
-    return response.ok ? response.json() : (function () { throw Error(response.statusText) }())
-  })
+'use strict';
 
-  .then(data => {
-    //TODO:
-    //call graph plotting functions
-    console.log(data)
-  })
+const country_code_to_emoji = country_code => {
+    const code_points = country_code
+        .toUpperCase()
+        .split('')
+        // Flag emojis are just country codes, but written using special unicode region characters
+        // 127462 is the code point for 'A' in the unicode region characters
+        // we subtract 65 because 65 is 'A' as a regular letter, and we need it to be 0th character
+        .map(char => 127462 + char.charCodeAt() - 65)
+    return String.fromCodePoint(...code_points)
+}
 
-  .catch(error => {
-    console.log("fetch error")
-  })
+const create_data_showcase = data => {
+    const data_container = document.querySelector("#data-container");
+    for (const data_point of data) {
 
-cursor = document.querySelector("#cursor")
+        const add_node = (name, parent, data_source, set_text=true) => {
+            const new_node = document.createElement("div");
+            parent.appendChild(new_node);
+            new_node.classList.add(name);
+            new_node.textContent = set_text ? data_source[name] : '';
+            return new_node;
+        }
+
+        const data_parent = add_node("data-point", data_container, false);
+
+        const add_data_node = (name, set_text=true) => { return add_node(name, data_parent, data_point, set_text) }
+
+        add_data_node("username");
+        add_data_node("email");
+        add_data_node("age");
+
+        const nationality = add_data_node("nationality", false);
+        nationality.textContent = country_code_to_emoji(data_point["nationality"]);
+
+        const website_visits = add_data_node("website_visits", false);
+
+        for (const website_visit of data_point["website_visits"]) {
+            const website_visit_node = add_node("website-visit", website_visits, false);
+
+            const add_visit_node = (name, set_text=true) => { return add_node(name, website_visit_node, website_visit, set_text) }
+            
+            add_visit_node("id");
+            add_visit_node("browser");
+            add_visit_node("ip_address");
+            add_visit_node("entry_date");
+            add_visit_node("entry_time");
+            add_visit_node("session_length");
+        }
+    }
+}
+
+const fetch_data = () => {
+    fetch('https://my.api.mockaroo.com/website_entries.json?key=7d9d28a0')
+        .then(response => {
+            return response.ok ? response.json() : (() => { throw Error(response.statusText) })();
+        })
+
+        .then(data => {
+            create_data_showcase(data)
+        })
+
+        .catch(error => {
+            console.log("fetch error");
+        })
+}
+
+document.querySelector("body").onload = fetch_data
+
+let cursor = document.querySelector("#cursor")
 document.querySelector("body").onmousemove = (e) => {
-    cursor.style.top = `${e.clientY - cursor.offsetHeight / 2}px`
-    cursor.style.left = `${e.clientX - cursor.offsetWidth / 2}px`
+    // e.clientY is the Y position of the mouse cursor.
+    // cursor.offsetHeight is the height of the circular cursor in pixels
+    // half of it is subtracted from the Y position so the circular cursor is centered on the real cursor
+    cursor.style.top = `${e.clientY - cursor.offsetHeight / 2}px`;
+    cursor.style.left = `${e.clientX - cursor.offsetWidth / 2}px`;
 }
